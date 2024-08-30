39f74397582baa6cb16c2bfda268c1a1e06d199d
Stanislav Chyburiak <stanislav.chyburiak@gmail.com>
2024-08-30 01:29:54 +0100

    All layouts prepared.

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,19 @@
+<x-panel class="flex gap-x-6">
+    <div>
+        <x-employer-logo/>
+    </div>
+
+    <div class="flex-1 flex flex-col">
+        <a href="#!" class="self-start text-sm text-gray-400">Company</a>
+
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-600 transition-colors duration-300">Position Title</h3>
+
+        <p class="text-sm text-gray-400 mt-auto">Description</p>
+    </div>
+
+    <div>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+    </div>
+</x-panel>
